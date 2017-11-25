9b0162aff61c37fb2bb47859a40793f6d7acb259
youthZ <552099787@qq.com>
2017-11-25 15:43:54 +0000

    finished

--- main/main.js
+++ main/main.js
@@ -1,6 +1,30 @@
 // Write your cade below:
-function main() {};
+function main(numberA, numberB) {
+	var nA = stringToNumber(numberA);
+	var nB = stringToNumber(numberB);
+	console.log(nA);
+	console.log(nB);
+	var countA = 0;
+	var countB = 0;
 
-module.exports = {
-    main
+	nB.forEach(function(ele){
+		if(nA.indexOf(ele) >= 0){
+			if(nA.indexOf(ele) == nB.indexOf(ele)){
+				countA ++;
+			}else{
+				countB++;
+			}
+		}
+			
+	});
+	return countA.toString()+'A'+countB.toString()+'B';
+};
+function stringToNumber(str){
+	var temp = str.split('');
+	for (var index = 0; index < temp.length ; index++)
+	{
+		temp[index] = parseInt(temp[index]);
+	}
+	return temp;
 }
+module.exports = main;
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,19 @@
 const main = require('../main/main');
 
 describe('main()', () => {
+	it('shoule guess number correct number and position',function(){
+		var randomNumber = '1234';
+		var guessNumber = '1234';
+		var result = main(randomNumber,guessNumber);
+		var expected = '4A0B'
+		expect(result).toEqual(expected);
+	});
 
+	it('shoule guess number correct number and position',function(){
+		var randomNumber = '1234';
+		var guessNumber = '4321';
+		var result = main(randomNumber,guessNumber);
+		var expected = '0A4B'
+		expect(result).toEqual(expected);
+	});
 });
